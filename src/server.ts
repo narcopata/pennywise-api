96048c6b77d550e8d8ce4bee3bea1cef9637b77a
RWS <rws@core>
2024-03-25 11:29:54 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+
+import Koa from "koa";
+import koaBody from "koa-body";
+
+import cors from "@koa/cors";
+import { router } from "./app/routes";
+
+const server = new Koa();
+
+const PORT = process.env.APP_PORT || 3333;
+
+server.use(
+	cors({
+		origin: "*",
+	}),
+);
+
+server.use(koaBody());
+
+server.use(router.routes()).use(router.allowedMethods());
+
+server.listen(PORT, () => {
+	console.log("Server running on port " + "\x1b[36m" + PORT + "\x1b[0m");
+});
