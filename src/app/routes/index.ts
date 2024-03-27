a3ef41598fa1569a66c913f690e94ddec535bf0c
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-03-26 23:24:37 -0300

    refactor: adding new routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,8 @@
 import Router from "@koa/router";
+import { authRouter } from "./auth.routes";
 
 const router = new Router();
+
+router.use("/auth", authRouter.routes(), authRouter.allowedMethods());
 
 export { router };
